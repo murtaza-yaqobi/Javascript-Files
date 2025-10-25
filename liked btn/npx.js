ecebf565a593aa09b268b683b4c43693cd27a6fb
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-10-25 07:24:29 +0430

    Added

--- liked btn/npx.js
+++ liked btn/npx.js
@@ -83,7 +83,8 @@ for (let i = 0; i < like2Buttons.length; i++) {
     }
   });
 }
-// 3
+// 3 div
+// 3 div
 for (let i = 0; i < like3Buttons.length; i++) {
   const btn = like3Buttons[i];
   const wrapper = btn.parentElement;
